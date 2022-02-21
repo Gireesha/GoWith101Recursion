826d2e123c895486d992a765a11ff43d49a9eef1
Gireesha <Gireesha@users.noreply.github.com>
2022-02-21 12:53:31 -0600

    Added printOddNEvenNumbers_005

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ func main() {
 	//fmt.Println(printNaturalNumbersDesc_002(12))   // 12 ==> "12 11 10 9 8 7 6 5 4 3 2 1"
 	//fmt.Println(printNaturalNumbersAsc_003(12))    // 12 ==> "1 2 3 4 5 6 7 8 9 10 11 12"
 
-	arr := [10]float64{67.8, 29.23, 35.22, 784.19, 5.6, 34.48, 39.55, 67.33, 689.33, 6.37}
-	fmt.Println(printSmallestNumberInAnArray_004(arr[:], 10)) // [] ==> 5.6
+	//arr := [10]float64{67.8, 29.23, 35.22, 784.19, 5.6, 34.48, 39.55, 67.33, 689.33, 6.37}
+	//fmt.Println(printSmallestNumberInAnArray_004(arr[:], 10)) // [] ==> 5.6
 
+	printOddNEvenNumbers_005(1, 3) // 1 is Odd number, 2 is Even number, 3 is Odd number
 }
 
 func reverseString_001(raw string) (reversed string) {
@@ -62,4 +63,28 @@ func printSmallestNumberInAnArray_004(arrInput []float64, size int) float64 {
 	}
 	//fmt.Println("arrInput[size-1] = ", arrInput[size-1], "size = ", size)
 	return math.Min(arrInput[size-1], printSmallestNumberInAnArray_004(arrInput, size-1))
+}
+
+func printOddNEvenNumbers_005(startNumber int, endNumber int) {
+	//Print the odd and even # between the number range(startNumber and endNumber)
+	//printOddNEvenNumbers_005(1,4) = printOddNEvenNumbers_005(2,4)
+	//printOddNEvenNumbers_005(2,4) = printOddNEvenNumbers_005(3,4)
+	//printOddNEvenNumbers_005(4, 4) = print; Base condition
+
+	if startNumber == endNumber {
+		if startNumber%2 == 0 {
+			fmt.Println(startNumber, "is Even number")
+		} else {
+			fmt.Println(startNumber, "is Odd number")
+		}
+		return
+	}
+
+	if startNumber%2 == 0 {
+		fmt.Println(startNumber, "is Even number")
+	} else {
+		fmt.Println(startNumber, "is Odd number")
+	}
+
+	printOddNEvenNumbers_005(startNumber+1, endNumber)
 }
